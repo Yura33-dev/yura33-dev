d1f135b598213dc764ed9261d34a4c7105736f40
Yura Kasyan <33.yurakasyan@gmail.com>
2024-04-28 20:38:52 +0200

    add lecture 34 (blog)

--- exercises/blog/src/App.jsx
+++ exercises/blog/src/App.jsx
@@ -1,36 +1,14 @@
 import './App.css';
-
-const jsxElement = <h1>I am a JSX element</h1>;
-
-const header = (
-  <header>
-    <h1>Welcome to React</h1>
-    <h2>Getting Started React</h2>
-    <h3>JavaScript Library</h3>
-  </header>
-);
-
-const anotherFragment = (
-  <>
-    <h1>Welcome to React</h1>
-    <h2 style={{ color: 'red' }}>Getting Started React</h2>
-    <h3>JavaScript Library</h3>
-  </>
-);
-
-const footer = (
-  <footer>
-    <p>Copyright &copy; 2024</p>
-  </footer>
-);
+import Footer from './components/Footer';
+import Gallery from './components/Gallery';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      {jsxElement}
-      {header}
-      {anotherFragment}
-      {footer}
+      <Navbar />
+      <Gallery />
+      <Footer />
     </>
   );
 }
